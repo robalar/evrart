d012b065ec1c3cf5aff44204c8de7d5ad47c6fc3
Robert Hales <rbthales@gmail.com>
2023-11-13 18:26:32 +0000

    Decode request

--- src/docker_hub.rs
+++ src/docker_hub.rs
@@ -1,14 +1,20 @@
+#[derive(serde::Deserialize, Debug, Clone)]
+struct AuthResponse {
+    token: String,
+}
+
 pub fn download_image(image: &str) {
     let client = reqwest::blocking::Client::new();
 
     // Get auth token for docker registry
-    let auth_response = client
+    let auth_response: AuthResponse = client
+        //             ^^^^^^^^^^^^ new!
         .get("https://auth.docker.io/token?service=registry.docker.io")
         .query(&[("scope", format!("repository:library/{}:pull", image))])
         .send()
         .unwrap()
-        .text()
+        .json()  // used to be .text()
         .unwrap();
 
-    dbg!(auth_response);
+    dbg!(auth_response.token);
 }
